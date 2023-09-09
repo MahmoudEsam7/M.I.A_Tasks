1e7aa2c92f8aa145ae9c3499182d5165ae6c12ba
MahmoudEsam <108243580+MahmoudEsam7@users.noreply.github.com>
2023-09-10 00:15:54 +0300

    Update O_encoder.ino

--- O_encoder.ino
+++ O_encoder.ino
@@ -1,8 +1,14 @@
 /**********************************************Macros Start*****************************************************/
 #define Encoder_A 2
 #define Encoder_B 3
+#define input_weight 0.731
+#define out_weight 0.462
 /**********************************************Macros End*******************************************************/
 double count = 0; //record number of pulses received
+unsigned int input_A[2] {0,0};
+unsigned int input_B[2] {0,0};
+unsigned int out_A[2] {0,0};
+unsigned int out_B[2] {0,0};
 
 /***************************************Function Deceleration Start*********************************************/
 void doencoder_A(void);
@@ -20,13 +26,25 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  //low pass filter
+  input_A[1] = digitalRead(Encoder_A);
+  input_B[1] = digitalRead(Encoder_B);
+  out_A[1]   = out_weight*out_A[0] + input_weight*(input_A[1]+input_A[0]);
+  out_B[1]   = out_weight*out_B[0] + input_weight*(input_B[1]+input_B[0]);
+  //update 
+  input_A[0] = input_A[1];
+  input_B[0] = input_B[1];
+  out_A[0]   = out_A[1];
+  out_B[0]   = out_B[1];
+  delay(20);
   Serial.print("Counts:\t");
   Serial.println(count);
 }
 
 /***************************************Function Definition Start********************************************/
 void doencoder_A(void){
+
   if (digitalRead(Encoder_A) != digitalRead(Encoder_B))
   {
     count++;
