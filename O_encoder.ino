92bb6dd4617bb737b8e05f9c4f2c7ca1ce938ac2
MahmoudEsam <108243580+MahmoudEsam7@users.noreply.github.com>
2023-09-13 18:28:07 +0300

    Update O_encoder.ino

--- O_encoder.ino
+++ O_encoder.ino
@@ -1,66 +1,58 @@
 /**********************************************Macros Start*****************************************************/
 #define Encoder_A 2
 #define Encoder_B 3
-#define input_weight 0.731
-#define out_weight 0.462
+#define b 0.9829
+#define a 0.01705 // Adjust this value to control the amount of filtering.
 /**********************************************Macros End*******************************************************/
-double count = 0; //record number of pulses received
-unsigned int input_A[2] {0,0};
-unsigned int input_B[2] {0,0};
-unsigned int out_A[2] {0,0};
-unsigned int out_B[2] {0,0};
+
+double raw_count = 0;       // Record the raw count from the encoder.
+double filtered_count = 0; // Record the filtered count from the encoder.
 
 /***************************************Function Deceleration Start*********************************************/
-void doencoder_A(void);
-void doencoder_B(void);
-/***************************************F*unction Deceleration End**********************************************/
+void do_encoder_a(void);
+void do_encoder_b(void);
+/***************************************Function Deceleration End**********************************************/
 
 void setup() {
-  
+
   Serial.begin(9600);
-  pinMode(Encoder_A,INPUT_PULLUP);   
-  pinMode(Encoder_B,INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(Encoder_A),doencoder_A,CHANGE);
-  attachInterrupt(digitalPinToInterrupt(Encoder_B),doencoder_B,CHANGE);
+  pinMode(Encoder_A, INPUT_PULLUP);
+  pinMode(Encoder_B, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(Encoder_A), do_encoder_a, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(Encoder_B), do_encoder_b, CHANGE);
 
 }
 
 void loop() {
-  
-  //low pass filter
-  input_A[1] = digitalRead(Encoder_A);
-  input_B[1] = digitalRead(Encoder_B);
-  out_A[1]   = out_weight*out_A[0] + input_weight*(input_A[1]+input_A[0]);
-  out_B[1]   = out_weight*out_B[0] + input_weight*(input_B[1]+input_B[0]);
-  //update 
-  input_A[0] = input_A[1];
-  input_B[0] = input_B[1];
-  out_A[0]   = out_A[1];
-  out_B[0]   = out_B[1];
-  delay(20);
-  Serial.print("Counts:\t");
-  Serial.println(count);
+  // Put your main code here, to run repeatedly:
+  Serial.print("Raw Counts:\t");
+  Serial.println(raw_count);
+  Serial.print("Filtered Counts:\t");
+  Serial.println(filtered_count);
 }
 
 /***************************************Function Definition Start********************************************/
-void doencoder_A(void){
+void do_encoder_a(void) {
 
-  if (out_A[1] != out_B[1])
-  {
-    count++;
-  }else
-  {
-    count--;
+  if (digitalRead(Encoder_A) != digitalRead(Encoder_B)) {
+    raw_count++;
+  } else {
+    raw_count--;
   }
+
+  // Apply the low-pass filter.
+  filtered_count = (a * filtered_count) + (b * raw_count);
 }
 
-void doencoder_B(void){
-  if (out_A[1] == out_B[1])
-  {
-    count++;
-  }else
-  {
-    count--;
+void do_encoder_b(void) {
+
+  if (digitalRead(Encoder_A) == digitalRead(Encoder_B)) {
+    raw_count++;
+  } else {
+    raw_count--;
   }
+
+  // Apply the low-pass filter.
+  filtered_count = (a * filtered_count) + (b * raw_count);
 }
 /****************************************Function Definition End*********************************************/
