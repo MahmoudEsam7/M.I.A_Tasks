160deeb4e6c61e875613f48b765b5121c3b3f9aa
MahmoudEsam <108243580+MahmoudEsam7@users.noreply.github.com>
2023-09-14 08:19:37 +0300

    Update O_encoder.ino

--- O_encoder.ino
+++ O_encoder.ino
@@ -1,16 +1,17 @@
 /**********************************************Macros Start*****************************************************/
 #define Encoder_A 2
 #define Encoder_B 3
-#define b 0.9829
-#define a 0.01705 // Adjust this value to control the amount of filtering.
+#define b 0.9956
+#define a 0.004389 // Adjust this value to control the amount of filtering.
 /**********************************************Macros End*******************************************************/
 
-double raw_count = 0;       // Record the raw count from the encoder.
-double filtered_count = 0; // Record the filtered count from the encoder.
+double raw_count[2] = {0,0}; // Record the raw count from the encoder.
+double filtered_count[2] = {0,0}; // Record the filtered count from the encoder.
 
 /***************************************Function Deceleration Start*********************************************/
 void do_encoder_a(void);
 void do_encoder_b(void);
+void low_pass_filter(void);
 /***************************************Function Deceleration End**********************************************/
 
 void setup() {
@@ -26,33 +27,41 @@ void setup() {
 void loop() {
   // Put your main code here, to run repeatedly:
   Serial.print("Raw Counts:\t");
-  Serial.println(raw_count);
+  Serial.println(raw_count[1]);
   Serial.print("Filtered Counts:\t");
-  Serial.println(filtered_count);
+  Serial.println(filtered_count[1]);
 }
 
 /***************************************Function Definition Start********************************************/
 void do_encoder_a(void) {
 
   if (digitalRead(Encoder_A) != digitalRead(Encoder_B)) {
-    raw_count++;
+    raw_count[1]+= 1;
   } else {
-    raw_count--;
+    raw_count[1]-= 1;
   }
 
   // Apply the low-pass filter.
-  filtered_count = (a * filtered_count) + (b * raw_count);
+  low_pass_filter();
 }
 
 void do_encoder_b(void) {
 
   if (digitalRead(Encoder_A) == digitalRead(Encoder_B)) {
-    raw_count++;
+    raw_count[1]+= 1;
   } else {
-    raw_count--;
+    raw_count[1]-= 1;
   }
 
   // Apply the low-pass filter.
-  filtered_count = (a * filtered_count) + (b * raw_count);
+  low_pass_filter();
 }
+
+void low_pass_filter(void)
+{
+    filtered_count[1] = (a * filtered_count[0]) + (b *(raw_count[1] + raw_count[0]));
+    filtered_count[0]= filtered_count[1];
+    raw_count[0] = raw_count[1];  
+}
+  
 /****************************************Function Definition End*********************************************/
